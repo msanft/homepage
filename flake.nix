ef6a895adf750fb215133f57b1346731127a0e14
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2025-08-13 10:28:23 +0200

    chore: update

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
       {
         packages.default = pkgs.stdenvNoCC.mkDerivation {
           name = "homepage";
